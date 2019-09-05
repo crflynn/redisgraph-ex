b7f58de8afdd33c45c39b38418d688ae10b83329
Flynn <crf204@gmail.com>
2019-09-05 01:34:55 -0400

    initial write

---
+++ lib/redis_graph/edge.ex
@@ -0,0 +1,39 @@
+defmodule RedisGraph.Edge do
+  alias RedisGraph.Util
+
+  @enforce_keys [:src_node, :dest_node]
+  defstruct [:id, :src_node, :dest_node, relation: "", properties: %{}]
+
+  def new(map) do
+    struct(__MODULE__, map)
+  end
+
+  def properties_to_string(edge) do
+    inner =
+      Map.keys(edge.properties)
+      |> Enum.map(fn key -> "#{key}:#{Util.quote_string(edge.properties[key])}" end)
+      |> Enum.join(",")
+
+    "{" <> inner <> "}"
+  end
+
+  def to_query_string(edge) do
+    src_node_string = "(" <> edge.src_node.alias <> ")"
+    edge_string = "-[:" <> edge.relation <> properties_to_string(edge) <> "]->"
+    dest_node_string = "(" <> edge.dest_node.alias <> ")"
+
+    src_node_string <> edge_string <> dest_node_string
+  end
+
+  def left == right do
+    cond do
+      !is_nil(left.id) && !is_nil(right.id) && Kernel.==(left.id, right.id) -> true
+      left.src_node != right.src_node -> false
+      left.dest_node != right.dest_node -> false
+      left.relation != right.relation -> false
+      map_size(left.properties) != map_size(right.properties) -> false
+      !Map.equal?(left.properties, right.properties) -> false
+      true -> true
+    end
+  end
+end
