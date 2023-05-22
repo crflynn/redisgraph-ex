143f3de6637303f67c783b91c8ed47bb85fd5e5d
AlexSandro19 <a.sandrovschii@gmail.com>
2023-05-22 23:40:43 +0200

    added adiditional utility functions

--- lib/redis_graph/util.ex
+++ lib/redis_graph/util.ex
@@ -34,4 +34,42 @@ defmodule RedisGraph.Util do
       ""
     end
   end
+
+  def properties_to_string(properties) do
+    props = cond do
+      is_map(properties) and map_size(properties) > 0 -> Stream.map(properties, fn({k, x}) -> "#{k}: #{converted_value(x)}" end) |> Enum.join(", ")
+      :true -> ""
+    end
+    IO.inspect(props)
+    case String.length(props) do
+      0 -> ""
+      _ -> "{#{props}}"
+    end
+  end
+
+  def labels_to_string(labels) do
+    cond do
+      is_list(labels) and length(labels) > 0 -> ":" <> Enum.join(labels, ":")
+      :true -> ""
+    end
+  end
+
+  def type_to_string(type) do # need to check if if can have multiple types
+    cond do
+      is_binary(type) and String.length(type) > 0 -> ":" <> type
+      :true -> ""
+    end
+  end
+
+  def converted_value(val) do
+    IO.inspect(val)
+    cond do
+      is_binary(val) ->  "'" <> val <> "'"
+      is_number(val) -> val
+      is_boolean(val) -> "#{val}"
+      is_nil(val) -> "null"
+      true -> "value type is not supported"
+    end
+  end
+
 end
