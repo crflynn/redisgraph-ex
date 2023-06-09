8f47829917b9ae39e395d5ea2989724c31ae992e
AlexSandro19 <a.sandrovschii@gmail.com>
2023-06-09 13:59:09 +0200

    changed docs for redis_graph

--- lib/redis_graph.ex
+++ lib/redis_graph.ex
@@ -1,4 +1,75 @@
 defmodule RedisGraph do
+  @moduledoc """
+
+  Query builder that provides functions
+  to construct Cypher queries for for RedisGraph database
+  and interact with the entities through defined structures.
+
+  The library is developed on top of an existing [library](https://github.com/crflynn/redisgraph-ex)
+  and provides additional functionality with refacted codebase to support
+  [RedisGraph result set](https://redis.io/docs/stack/graph/design/client_spec/).
+
+  To launch ``redisgraph`` locally with Docker, use
+
+  ```bash
+  docker run -p 6379:6379 -it --rm redis/redis-stack-server
+  ```
+
+  Here is a simple example:
+
+  ```elixir
+  alias RedisGraph.{Query, Graph, QueryResult}
+
+  # Create a connection using Redix
+  {:ok, conn} = Redix.start_link("redis://localhost:6379")
+
+  # Create a graph
+  graph = Graph.new(%{
+    name: "social"
+  })
+
+  {:ok, query} =
+        Query.new()
+        |> Query.create()
+        |> Query.node(:n, ["Person"], %{age: 30, name: "John Doe", works: true})
+        |> Query.relationship_from_to(:r, "TRAVELS_TO", %{purpose: "pleasure"})
+        |> Query.node(:m, ["Place"], %{name: "Japan"})
+        |> Query.return(:n)
+        |> Query.return_property(:n, "age", :Age)
+        |> Query.return(:m)
+        |> Query.build_query()
+
+  # query will hold
+  # "MATCH "MATCH (n:Person {age: 30, name: 'John Doe', works: true})-[r:TRAVELS_TO {purpose: 'pleasure'}]->(m:Place {name: 'Japan'}) RETURN n, n.age AS Age, m"
+
+  # Execute the query
+  {:ok, query_result} = RedisGraph.query(conn, graph.name, query)
+
+  # Get result set
+  result_set = Map.get(query_result, :result_set)
+  # result_set will hold
+  # [
+  #   [
+  #     %RedisGraph.Node{
+  #       id: 2,
+  #       alias: :n,
+  #       labels: ["Person"],
+  #       properties: %{age: 30, name: "John Doe", works: true}
+  #     },
+  #     30,
+  #     %RedisGraph.Node{
+  #       id: 3,
+  #       alias: :m,
+  #       labels: ["Place"],
+  #       properties: %{name: "Japan"}
+  #     }
+  #   ]
+  # ]
+
+  ```
+
+
+  """
   alias RedisGraph.Relationship
   alias RedisGraph.Graph
   alias RedisGraph.Node
